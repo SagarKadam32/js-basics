bd31a209f5ab2023fb94d174acd53e19628776b9
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-24 20:40:45 +0530

    Primitive Types

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ console.log('Hello World!');
 let firstName = 'Sagar';
 let lastName = 'Kadam';
 const piValue = 3.14;
-piValue = 2.3;
+// piValue = 2.3;  //
+
+// Primitive Types
+let myName = 'Sagar'; //String Literal
+let age = 41; // Number Literal
+let isAwesome = true; // Boolean
+let boxItem = undefined;
+let selectedColor = null;
+age = 32.34;
