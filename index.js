f72e8bd29d32dd5f9db59904152a51092ff30f7c
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-25 09:18:43 +0530

    String Properties

--- index.js
+++ index.js
@@ -230,3 +230,26 @@ function countTruthy(array) {
     }
     console.log('Truthy Values = ', truthy);
 }
+
+console.log('Show Properties')
+
+const movie = {
+    title: 'a',
+    releaseYear: 2018,
+    rating: 4.5,
+    director: 'b'
+}
+
+showProperties(movie);
+
+function showProperties(obj) {
+    for(key in obj) {
+        console.log(key, obj[key]);
+    }
+
+    for(key in obj) {
+        if(typeof obj[key] === 'string') {
+            console.log(key, obj[key]);
+        }
+    }
+}
