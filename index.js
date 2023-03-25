cc79930cba212c3ddf217088c6704e42afbc13db
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-25 09:24:39 +0530

    Sum of Multiple of 3 and 5

--- index.js
+++ index.js
@@ -253,3 +253,20 @@ function showProperties(obj) {
         }
     }
 }
+
+console.log('Sum of Multiple of 3 and 5');
+// limit => 10
+// Multiple of 3 => 3,6,9
+// Multiple of 5 => 5,10
+
+console.log(sum(10));
+
+function sum(limit) {
+    let sumAmount = 0;
+    for (let i=1; i<= limit; i++) {
+        if (i%3 === 0 || i%5 === 0) {
+            sumAmount += i;
+        }
+    }
+    return sumAmount;
+}
