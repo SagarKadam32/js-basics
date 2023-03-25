e33aafb86f56814c67833868626e6b3c96bca7c4
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-25 09:46:53 +0530

    Average Better code

--- index.js
+++ index.js
@@ -281,29 +281,22 @@ console.log('Excercise - Grades');
 // 80-89 : B
 // 90-100 : A
 
-const marks = [80, 80, 50];
+const marks = [90, 96, 89];
 console.log(calculateGrade(marks));
 
 function calculateGrade(marks) {
-    let average = 0;
-    let totalMarks = 0;
-    let subjectCount = 0;
+    const average = calculateAverage(marks);
+    if(average < 60) return 'F';
+    if(average < 70) return 'D';
+    if(average < 80) return 'C';
+    if(average < 90) return 'B';
+    return 'A'    
+}
 
-    for(score of marks) {
-        totalMarks += score;
-        subjectCount += 1;
+function calculateAverage(array) {
+    let sum = 0;
+    for (let value of array) {
+        sum += value
     }
-    average = totalMarks/subjectCount;
-
-    if(average >= 1 && average <= 59) {
-        return 'F'
-    }else if(average >= 60 && average <= 69) {
-        return 'D'
-    }else if(average >= 70 && average <= 79) {
-        return 'C'
-    }else if(average >= 80 && average <= 89) {
-        return 'B'
-    }else if(average >= 90 && average <= 100) {
-        return 'A'
-    }
+    return sum / array.length;
 }
